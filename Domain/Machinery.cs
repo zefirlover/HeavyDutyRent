e2b62cfe6739e3552ef5d62467a58248bf15c889
zefirlover <ibodikmushta@gmail.com>
2023-08-03 17:27:44 +0300

    Initial Migration, update Machinery, add start for seeding data

--- Domain/Machinery.cs
+++ Domain/Machinery.cs
@@ -5,12 +5,12 @@ public class Machinery
     public int Id { get; set; }
     public string Name { get; set; }
     public string AddressLine { get; set; }
-    public List<string>? ImagesUrls { get; set; }
     public string Price { get; set; }
     
     public int SellerId { get; set; }
     public Seller Seller { get; set; }
     
+    public ICollection<Image> ImagesUrls { get; set; }
     public ICollection<Category> Categories { get; set; }
     public ICollection<Order> Orders { get; set; }
 }
