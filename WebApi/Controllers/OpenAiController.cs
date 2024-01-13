cc716ca0efc10e76a3faa461279d7c2ebc6a782b
zefirlover <ibodikmushta@gmail.com>
2024-01-13 16:50:11 +0200

    Update OpenAiControllers method and model

--- WebApi/Controllers/OpenAiController.cs
+++ WebApi/Controllers/OpenAiController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using OpenAI_API;
+using OpenAI_API.Chat;
 using OpenAI_API.Completions;
 using Swashbuckle.AspNetCore.Annotations;
 
@@ -38,15 +39,28 @@ public class OpenAiController : ControllerBase
         var completionRequest = new CompletionRequest
         {
             Prompt = promptWithContext,
-            Model = OpenAI_API.Models.Model.DavinciText,
+            Model = OpenAI_API.Models.Model.CurieText,
             MaxTokens = 256
         };
 
-        var completions = await openai.Completions.CreateCompletionAsync(completionRequest);
-
-        foreach (var completion in completions.Completions)
+        var result = await openai.Chat.CreateChatCompletionAsync(new ChatRequest()
         {
-            outputResult += completion.Text;
+            Model = OpenAI_API.Models.Model.ChatGPTTurbo,
+            MaxTokens = 50,
+            Temperature = 0.5,
+            Messages = new ChatMessage[]
+            {
+                new (ChatMessageRole.System, "You are a helpful assistant."),
+                new (ChatMessageRole.System, "You are working on online service for renting construction, work and agricultural equipment"),
+                new (ChatMessageRole.System, "Do not mention availability or location-based variances in your responses."),
+                //new (ChatMessageRole.System, "On 'Thank you' you need to answer 'You are welcome.' without additional sentence."),
+                new (ChatMessageRole.User, query)
+            }
+        });
+
+        foreach (var completion in result.Choices)
+        {
+            outputResult += completion.Message.Content.Trim();
         }
 
         return Ok(outputResult);
